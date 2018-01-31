a7a37ccdf421eb8fc28e347ed60335ed50160b05
Noni Wulandari <6002noniwulandari@gmail.com>
2018-01-31 17:13:23 +0700

    update sampe list recyclerview

--- app/src/main/java/camp/bso/inf/scoringboard/ListPertandingan.java
+++ app/src/main/java/camp/bso/inf/scoringboard/ListPertandingan.java
@@ -9,6 +9,5 @@ public class ListPertandingan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_pertandingan);
-        //asasasasadasdsdsdsasasddsasasd
     }
 }
