5a34a502e26102e88ccbba6f6dd41ec4f285d10d
Noni Wulandari <6002noniwulandari@gmail.com>
2018-02-01 17:31:20 +0700

    update sampe list navbar

--- app/src/main/java/camp/bso/inf/scoringboard/ListPertandingan.java
+++ app/src/main/java/camp/bso/inf/scoringboard/ListPertandingan.java
@@ -2,12 +2,49 @@ package camp.bso.inf.scoringboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.LinkedList;
+
+import camp.bso.inf.scoringboard.R;
 
 public class ListPertandingan extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList1 = new LinkedList<>();
+    private final LinkedList<String> mWordList2 = new LinkedList<>();
+    private int mCount = 0;
+    String timkita[] = {"Persib", "Persib", "Persib"};
+    String timlawan[] = {"PSM", "Persija", "Persipura"};
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_pertandingan);
+
+        for (int i = 0; i < 3; i++) {
+            mWordList1.addLast(timkita[i]);
+            mWordList2.addLast(timlawan[i]);
+            Log.d("Wordlist1", mWordList1.getLast());
+            Log.d("Wordlist2", mWordList2.getLast());
+        }
+
+        //	Get	a	handle	to	the	RecyclerView.
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        //	Create	an	adapter	and	supply	the	data	to	be	displayed.
+        mAdapter = new WordListAdapter(this, mWordList1, mWordList2);
+
+        //	Connect	the	adapter	with	the	RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+
+        //	Give	the	RecyclerView	a	default	layout	manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
